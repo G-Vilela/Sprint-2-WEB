927447f9bc25594d83082e65a0613737cc132b21
Guilherme Vilela <guilhermepvilela@gmail.com>
2026-05-15 00:43:53 -0300

    feat: Slideshow JS

--- src/script.js
+++ src/script.js
@@ -28,3 +28,36 @@ btnLogin.addEventListener("click", function(){
     }
 
 });
+const imagens = [
+    "https://media.licdn.com/dms/image/v2/D4D05AQHIYsGf32mxuQ/feedshare-thumbnail_720_1280/feedshare-thumbnail_720_1280/0/1714661995954?e=2147483647&v=beta&t=SFF4nq814x_ns-XvAqC3X9oJnk-z5cw2o8XEZJIuNoU",
+    "https://s3.amazonaws.com/wpfiap/wp-content/uploads/2014/11/20141105_fiap_next_0489.jpg",
+    "https://i.ytimg.com/vi/7Ggx_UsW17o/maxresdefault.jpg"
+];
+
+let indice = 0;
+
+const slide = document.getElementById("slide");
+
+document.getElementById("proximo").addEventListener("click", function(){
+
+    indice++;
+
+    if(indice >= imagens.length){
+        indice = 0;
+    }
+
+    slide.src = imagens[indice];
+
+});
+
+document.getElementById("anterior").addEventListener("click", function(){
+
+    indice--;
+
+    if(indice < 0){
+        indice = imagens.length - 1;
+    }
+
+    slide.src = imagens[indice];
+
+});
